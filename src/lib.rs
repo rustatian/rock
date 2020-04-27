7b17412080d2d75e4f7056c9907a9ed13eea347f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-04-28 00:28:55 +0300

    update on reports

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![allow(dead_code)]
+
+use crate::errors::RockError;
+use crate::profile::Profile;
 use profile::buffer::ProfileDecoder;
 use std::path::Path;
 
@@ -11,40 +14,33 @@ mod reports;
 
 #[derive(Default)]
 pub struct Options {
-    binary_path: String,
+    profile_path: String,
 }
 
 fn pprof(op: &mut Options) {
-    if op.binary_path.is_empty() {
+    if op.profile_path.is_empty() {
         // this is warning, but ok
         eprintln!("binary path is empty")
     }
 
     // if wrong path provided -> panic
-    if !Path::exists(op.binary_path.as_ref()) {
-        panic!(format!("provided path {} does not exist", op.binary_path));
+    if !Path::exists(op.profile_path.as_ref()) {
+        panic!(format!("provided path {} does not exist", op.profile_path));
     }
 
-    load_binary(&op.binary_path);
+    let profile = load_binary(&op.profile_path).unwrap();
+    println!("{}", profile.to_string());
 }
 
-fn load_binary(path: &str) {
-    let binary = std::fs::read(path);
-    match binary {
-        Ok(data) => {
-            let p = profile::buffer::Buffer::unmarshal(data);
-            match p {
-                Ok(res) => {
-                    println!("{}", res.to_string());
-                }
-                Err(err) => {
-                    eprintln!("{}", err);
-                }
-            }
-        }
-        Err(err) => {
-            eprintln!("{}", err.to_string());
-        }
+fn load_binary(path: &str) -> Result<Profile, RockError> {
+    match std::fs::read(path) {
+        Ok(data) => match profile::buffer::Buffer::unmarshal(data) {
+            Ok(res) => Ok(res),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(RockError::Unknown {
+            reason: err.to_string(),
+        }),
     }
 }
 
@@ -53,7 +49,7 @@ mod lib_tests {
     #[test]
     fn main_tests() {
         let mut op = super::Options::default();
-        op.binary_path = "tests/CPU.pb.gz".to_string();
+        op.profile_path = "tests/CPU.pb.gz".to_string();
         super::pprof(&mut op);
     }
 }
--- src/reports/mod.rs
+++ src/reports/mod.rs
@@ -1,1 +1,29 @@
+use crate::profile::Profile;
+use crate::Options;
+
 mod graph;
+
+pub struct Report {
+    prof: Profile,
+    total: i64,
+    options: Options,
+    format_value: Box<dyn Fn(i64, Options) -> String>,
+}
+
+impl Report {
+    fn generate_raw_report(&self, p: Profile, opts: Options) -> Self {
+        Report {
+            prof: p,
+            total: 1,
+            options: opts,
+            format_value: Box::new(|num: i64, o: Options| {
+                let a = o.profile_path;
+                a
+            }),
+        }
+    }
+
+    fn aa(self) {
+        let ab = (self.format_value)(1, self.options);
+    }
+}
