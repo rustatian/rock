4c2213ee8b67c6ff1d13df0e47f9957b6da263a9
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-06-25 17:14:52 +0300

    update mimalloc declaration

--- rock_parser/src/lib.rs
+++ rock_parser/src/lib.rs
@@ -2,9 +2,9 @@
 
 pub mod profile;
 
-#[cfg(all(any(target_os = "linux", )))]
+#[cfg(target_os = "linux", )]
 use mi_malloc_rust::MiMalloc;
 
-#[cfg(all(any(target_os = "linux",)))]
+#[cfg(target_os = "linux",)]
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
