79e2f5ed7e5d8d7264a7a47acce82c17a5bd3862
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-17 00:07:55 +0300

    Actix web

--- crates/transport/src/main.rs
+++ crates/transport/src/main.rs
@@ -1,45 +1,16 @@
 #![warn(rust_2018_idioms)]
 
-use hyper::{body::HttpBody, Client};
-use tokio::io::{self, AsyncWriteExt as _};
+use actix_web::client::Client;
 
+#[actix_web::main]
+async fn main() {
+    let mut client = Client::default();
 
-// A simple type alias so as to DRY.
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+    // Create request builder and send request
+    let response = client.get("http://www.rust-lang.org")
+        .header("User-Agent", "actix-web/3.0")
+        .send()     // <- Send request
+        .await;     // <- Wait for response
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    pretty_env_logger::init();
-
-    let url = "http://spiralscout.com";
-
-    // HTTPS requires picking a TLS implementation, so give a better
-    // warning if the user tries to request an 'https' URL.
-    let url = url.parse::<hyper::Uri>().unwrap();
-    if url.scheme_str() != Some("http") {
-        println!("This example only works with 'http' URLs.");
-        return Ok(());
-    }
-
-    fetch_url(url).await
-}
-
-async fn fetch_url(url: hyper::Uri) -> Result<()> {
-    let client = Client::new();
-
-    let mut res = client.get(url).await?;
-
-    println!("Response: {}", res.status());
-    println!("Headers: {:#?}\n", res.headers());
-
-    // Stream the body, writing each chunk to stdout as we get it
-    // (instead of buffering and printing at the end).
-    while let Some(next) = res.data().await {
-        let chunk = next?;
-        io::stdout().write_all(&chunk).await?;
-    }
-
-    println!("\n\nDone!");
-
-    Ok(())
+    println!("Response: {:?}", response);
 }
