27c1a44cae57827fd6bd802767fff0176b6557a5
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-01-31 21:20:45 +0300

    node update

--- crates/core/src/graph/mod.rs
+++ crates/core/src/graph/mod.rs
@@ -1,4 +1,5 @@
 #![warn(missing_debug_implementations)]
+#![allow(dead_code)] //temporary
 
 // Graph summarizes a performance profile into a format that is
 // suitable for visualization.
@@ -9,7 +10,15 @@ struct Graph<'graph> {
 // Node is an entry on a profiling report. It represents a unique
 // program location.
 struct Node<'node> {
+    // Info describes the source location associated to this node.
     info: NodeInfo<'node>,
+
+    // Function represents the function that this node belongs to. On
+    // graphs with sub-function resolution (eg line number or
+    // addresses), two nodes in a NodeMap that are part of the same
+    // function have the same value of Node.Function. If the Node
+    // represents the whole function, it points back to itself.
+    function: &'node Node<'node>
 }
 
 // NodeInfo contains the attributes for a node.
