cd8557d9acb3a19f862fd65c95529c39dd21066d
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-01-31 23:41:44 +0300

    add edge, edgemap

--- crates/core/src/graph/mod.rs
+++ crates/core/src/graph/mod.rs
@@ -1,14 +1,20 @@
 #![warn(missing_debug_implementations)]
 #![allow(dead_code)] //temporary
+use std::collections::HashMap;
+
+// EdgeMap is used to represent the incoming/outgoing edges from a node.
+type EdgeMap<'a, 'b> = HashMap<Node<'a>, Edge<'b>>;
 
 // Graph summarizes a performance profile into a format that is
 // suitable for visualization.
+#[derive(Copy, Clone, Debug)]
 struct Graph<'graph> {
     nodes: &'graph Node<'graph>,
 }
 
 // Node is an entry on a profiling report. It represents a unique
 // program location.
+#[derive(Copy, Clone, Debug)]
 struct Node<'node> {
     // Info describes the source location associated to this node.
     info: NodeInfo<'node>,
@@ -29,6 +35,7 @@ struct Node<'node> {
 }
 
 // NodeInfo contains the attributes for a node.
+#[derive(Copy, Clone, Debug)]
 struct NodeInfo<'n> {
     name: &'n str,
     orig_name: &'n str,
@@ -37,4 +44,18 @@ struct NodeInfo<'n> {
     start_line: i64,
     lineno: i64,
     objfile: &'n str,
+}
+
+// Edge contains any attributes to be represented about edges in a graph.
+struct Edge<'edge> {
+    src: &'edge Node<'edge>,
+    dest: &'edge Node<'edge>,
+    // The summary weight of the edge
+    weight: i64,
+    weight_div: i64,
+    // residual edges connect nodes that were connected through a
+    // separate node, which has been removed from the report.
+    residual: bool,
+    // An inline edge represents a call that was inlined into the caller.
+    inline: bool,
 }
